9ae6dc25528f8c07ddfbec52c767088158a16f55
Glaucoed <gesverzuthe@gmail.com>
2024-01-27 19:10:50 -0300

    feat: adicionado conteudo sobre mim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,28 +13,58 @@ export default function Home() {
           </div>
         </Link>
       </div>
-      <div className="bg-zinc-200 py-10 gap-10 flex justify-center">
-        <div className="flex gap-10 p-10 w-2/3">
-          <div className="w-1/4">
+      <div className="bg-zinc-200 gap-10 flex justify-center flex-col p-20">
+        <div>
+        <h2 className="text-h1 uppercase font-bold text-escuro">Sobre mim</h2>
+        <div className="bg-dourado h-2 w-24" />
+
+        </div>
+        <div className="flex">
+          <div className="w-1/3">
             <Image
-              src="/Glauco.png"
-              width={200}
-              height={300}
-              className="rounded-3xl"
+              src="/teste.jpg"
+              width={500}
+              height={500}
               alt="Foto de Glauco"
             />
           </div>
-          <div className="flex flex-col gap-4 w-3/4">
-            <h2 className="text-h2 font-bold text-dourado">
-              Glauco Esverzuthe Damasceno
-            </h2>
+          <div className="flex flex-col gap-10 w-2/3">
+            <div className="w-1/2">
+              <h2 className="text-h2 font-bold text-escuro">
+                Glauco Esverzuthe Damasceno
+              </h2>
+              <p className="text-dourado font-bold text-center text-h4">Desenvolvedor Fullstack</p>
+            </div>
             <p className="w-2/3">
-              Me identifiquei com programação desde 2014, desde então cursei a
-              faculdade de Análise e Desenvolvimento de Sistemas em 2017,
-              desenvolvi diversos projetos durante a graduação, como front-end e
-              back-end, atualmente estou cursando a Trybe para ter um melhor
-              desenvolvimento e especialização nas linguagens atuais.
+            Desde 2014, minha afinidade com programação tem sido evidente. Em 2017, iniciei minha jornada acadêmica na faculdade de Análise e Desenvolvimento de Sistemas, onde participei ativamente do desenvolvimento de diversos projetos, abrangendo tanto a área de front-end quanto a de back-end. Posteriormente, busquei aprimorar minhas habilidades e conhecimentos ao ingressar na Trybe, onde concluí meu curso e me formei como desenvolvedor fullstack. Atualmente, atuo profissionalmente nas tecnologias Node.js e Next.js, buscando constantemente aprimorar minha expertise nesse campo dinâmico e em constante evolução.
             </p>
+            <div className="grid grid-cols-2 w-2/3 gap-5">
+              <div className="flex gap-2 border-b-2 border-escuro pb-2 mr-10">
+                <p className="text-h5 font-bold text-escuro">Contato: </p>
+                <p className="text-h5 text-escuro">11 00000-0000</p>                
+              </div>
+              <div className="flex gap-2 border-b-2 border-escuro pb-2 mr-10 ">
+                <p className="text-h5 font-bold text-escuro">Contato: </p>
+                <p className="text-h5 text-escuro">11 00000-0000</p>                
+              </div>
+              <div className="flex gap-2 border-b-2 border-escuro pb-2 mr-10">
+                <p className="text-h5 font-bold text-escuro">Contato: </p>
+                <p className="text-h5 text-escuro">11 00000-0000</p>                
+              </div>
+              <div className="flex gap-2 border-b-2 border-escuro pb-2 mr-10">
+                <p className="text-h5 font-bold text-escuro">Contato: </p>
+                <p className="text-h5 text-escuro">11 00000-0000</p>                
+              </div>
+              <div className="flex gap-2 border-b-2 border-escuro pb-2 mr-10">
+                <p className="text-h5 font-bold text-escuro">Contato: </p>
+                <p className="text-h5 text-escuro">11 00000-0000</p>                
+              </div>
+              <div className="flex gap-2 border-b-2 border-escuro pb-2 mr-10">
+                <p className="text-h5 font-bold text-escuro">Contato: </p>
+                <p className="text-h5 text-escuro">11 00000-0000</p>                
+              </div>
+              
+            </div>
           </div>
           </div>
         </div>
